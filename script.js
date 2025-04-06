81ec794eb5a58efbd7c8e8febeeb9bf4ee4fc276
manhnv193 <manhnv@fit-haui.edu.vn>
2025-04-07 00:48:52 +0700

    microtask run before macrotask

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 queueMicrotask(() => {
     console.log("microtask queueMicrotask");
+});
+
+setTimeout(() => {
+    console.log("macrotask setTimeout");
+}, 0);
+
+Promise.resolve().then(() => {
+    console.log("microtask promise");
 })
