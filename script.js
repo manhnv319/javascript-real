4787276fae92a84b0646b4f69a2bc7153abacb1e
manhnv193 <manhnv@fit-haui.edu.vn>
2025-04-07 01:16:48 +0700

    Implement user and post retrieval with Promise chaining in script.js

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const promise= new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("Thanh cong sau 2s");
-        
-    }, 2000);
-});
+function getUser() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Lay user...");
+            resolve({id: 1});
+        }, 1000);
+    });
+}
 
-promise.then((result) => {
-    console.log("Ket qua: ", result);
-}).catch((err) => {
-    console.log("Loi ", err);
-})
+function getPostByUser(userId) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Lay bai viet cua user...");
+            resolve(["post1", "post2", "post3"]);
+        }, 1000);
+    });
+}
+
+function sendResponse(post) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Da gui phan hoi voi", post);
+            resolve("Da gui!");
+        }, 1000);
+    });
+}
+
+// Chuoi Promise
+getUser()
+.then((user) => getPostByUser(user.id))
+.then((posts) => sendResponse(posts))
+.then((response) => console.log("ket qua", response))
+.catch((err => console.error("Co loi", err)))
