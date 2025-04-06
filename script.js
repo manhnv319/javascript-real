bc1b46436410657b72cc061a0d98ed54d0c27ec7
manhnv193 <manhnv@fit-haui.edu.vn>
2025-04-07 01:28:06 +0700

    Refactor script.js to use async/await for user retrieval and update notes in note.txt

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-function getUser() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("Lay user...");
-            resolve({id: 1});
-        }, 1000);
-    });
+async function myFunction() {
+    return "Hello, world!";
 }
 
-function getPostByUser(userId) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("Lay bai viet cua user...");
-            resolve(["post1", "post2", "post3"]);
-        }, 1000);
-    });
+myFunction().then(result => {
+    console.log(result);
+})
+
+
+async function getUser() {
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users/1')
+        if (!response.ok) {
+            throw new Error('Khong tim thay nguoi dunng');
+        }
+        const user = await response.json();
+        console.log(user);
+    }catch (error) {
+        console.error('Error:', error);
+    }
 }
 
-function sendResponse(post) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("Da gui phan hoi voi", post);
-            resolve("Da gui!");
-        }, 1000);
-    });
-}
-
-// Chuoi Promise
-getUser()
-.then((user) => getPostByUser(user.id))
-.then((posts) => sendResponse(posts))
-.then((response) => console.log("ket qua", response))
-.catch((err => console.error("Co loi", err)))
+getUser();
