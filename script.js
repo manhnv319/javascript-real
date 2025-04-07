f8d9e073ad41d73f43cd39db03039e507202d97f
manhnv193 <manhnv@fit-haui.edu.vn>
2025-04-07 09:26:25 +0700

    Refactor script.js to remove unused variables and demonstrate object destructuring with a printStudent function

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-const user = {
-    name: "Manh",
-    age: 22,
-    job: "Backend Dev"
-}
-
-// Cach cu
-const name1 = user.name;
-const age1 = user.age;
-
-// Destructuring
-const { name, age } = user;
-
-console.log(name);
-console.log(age);
-
-// Voi array 
-const arr = [10, 20, 30];
-
-// Cach cu
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
-
-// Destructuring
-const [x, y, z] = arr;
-console.log(x);
-console.log(y);
-
-
 // Spread/Rest
 const student = {
     id: 101,
@@ -38,6 +8,9 @@ const student = {
     }
 };
 
-const {id, nameStudent, scores} = student;
-const {math, english} = scores;
-console.log(nameStudent, math);
+function printStudent({nameStudent, scores: {math}}) {
+    console.log(`Ten: ${nameStudent}, Toan: ${math}`)
+}
+
+printStudent(student);
+
