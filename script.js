f1b926325329eb7403026491490585592cc456f0
manhnv193 <manhnv@fit-haui.edu.vn>
2025-04-07 10:27:57 +0700

    Refactor script.js to implement asynchronous data fetching with Promise.all

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const numbers = [1, 2, 3, 4, 5];
+async function fetchData(delay) {
+    return new Promise(resolve => setTimeout(() => resolve('Data fetched'), delay));
+}
 
-// map
-const doubled = numbers.map(num => num * 2);
-console.log(doubled);
+async function fetchAllData() {
+    const dataSources = [fetchData(1000), fetchData(2000), fetchData(3000), fetchData(4000)];
+    // let dataCount = 0;
+    // for await (const data of dataSources) {
+    //     dataCount++;
+    //     console.log(data + dataCount);
+    // }
 
-// filter
-const evenNumbers = numbers.filter(num => num % 2 === 0);
-console.log(evenNumbers);
+    const data = await Promise.all(dataSources);
+    console.log(data);
+}
 
-// reduce
-const sum = numbers.reduce((acc, num) => acc + num, 0);
-console.log(sum);
-
-// forEach
-numbers.forEach(num => {
-    console.log(num);
-})
-
+fetchAllData()
